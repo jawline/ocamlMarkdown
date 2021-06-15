d6816d8030264996baecdd5edf3eac9790a445f2
Blake Loring <blake@parsed.uk>
2021-06-15 10:19:38 +0800

    New tests, highlight an issue with blockquotes

--- src/to_html.ml
+++ src/to_html.ml
@@ -27,12 +27,68 @@ let rec to_html (markdown : Fragment.t) : string =
   | Code code_str -> sprintf "<code><pre>%s</pre></code>" code_str
   | Text text -> text
   | List (list_type, list_items) -> to_html_list list_type list_items
-  | Bold t -> sprintf "<b>%s<b>" (to_html t)
+  | Bold t -> sprintf "<b>%s</b>" (to_html t)
   | Italic t -> sprintf "<i>%s</i>" (to_html t)
-  | Heading (depth, title) -> sprintf "<h%i>%s</h%i>" depth title depth
+  | Heading (depth, title) -> sprintf "<h%i>%s</h%i>\n" depth title depth
   | Link (title, url) -> sprintf "<a href=\"%s\">%s</a>" url title
   | Blockquote t -> sprintf "<blockquote>%s</blockquote>\n" (to_html t)
   | HorizontalRule -> sprintf "<hr/>\n"
 ;;
 
-(* TODO: to_html tests *)
+exception ToHtmlTestingError of string
+
+let%test "basic_text" =
+  let parse = Parse.parse in
+  let output = to_html (parse "hello") in
+  if String.( = ) output "<p>hello</p>\n" then true else raise (ToHtmlTestingError output)
+;;
+
+let%test "bold_text" =
+  let parse = Parse.parse in
+  let output = to_html (parse "he**ll**o") in
+  if String.( = ) output "<p>he<b>ll</b>o</p>\n"
+  then true
+  else raise (ToHtmlTestingError output)
+;;
+
+let%test "italic_text" =
+  let parse = Parse.parse in
+  let output = to_html (parse "he*ll*o") in
+  if String.( = ) output "<p>he<i>ll</i>o</p>\n"
+  then true
+  else raise (ToHtmlTestingError output)
+;;
+
+let%test "heading" =
+  let parse = Parse.parse in
+  let output = to_html (parse "## Heading\n") in
+  if String.( = ) output "<h2>Heading</h2>\n"
+  then true
+  else raise (ToHtmlTestingError output)
+;;
+
+let%test "link" =
+  let parse = Parse.parse in
+  let output = to_html (parse "[Hello](https://example.com)\n") in
+  if String.( = ) output "<p><a href=\"https://example.com\">Hello</a></p>\n"
+  then true
+  else raise (ToHtmlTestingError output)
+;;
+
+let%test "basic_blockquote" =
+  let parse = Parse.parse in
+  let output = to_html (parse ">Hello\n>World") in
+  if String.( = ) output "<blockquote><p>Hello World</p>\n</blockquote>\n"
+  then true
+  else raise (ToHtmlTestingError output)
+;;
+
+let%test "two_paragraphs" =
+  let parse = Parse.parse in
+  let output = to_html (parse "# Heading\nHello World\n\nGoodbye World") in
+  if String.( = ) output "<h1>Heading</h1>\n<p>Hello World</p>\n<p>Goodbye World</p>\n"
+  then true
+  else raise (ToHtmlTestingError output)
+;;
+
+(* TODO: to_html tests are insufficient, missing important cases like Code*)
